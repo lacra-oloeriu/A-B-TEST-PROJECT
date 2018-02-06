26827d6f98ff20606a261a3d724e027a5563b753
lacra-oloeriu <lacramioara.oloeriu@gmail.com>
2018-02-06 20:07:07 +0100

    final submision and graduation from term 1. I'm super happy :)

---
+++ debug1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import random
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import sys
+
+print ("sys.executable = " + sys.executable)
+print ("End of debug")
